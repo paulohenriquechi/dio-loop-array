c2ee60404db5d6bb3935ffd3321acce351cbf8b2
Paulo <paulo.henrique.chi@outlook.com>
2022-10-31 23:35:55 -0300

    Exercicios Finalizados

---
+++ loop-array/src/ArrayEx04.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class ArrayEx04 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int matriz[][] = new int [4][4];
+        for(int i=0;i<matriz.length;i++){
+            for(int j=0;j<matriz[i].length;j++){
+                matriz[i][j] = random.nextInt(9);
+            }
+        }
+        System.out.println("Matriz: ");
+        for (int[] linha : matriz) {
+            for (int coluna : linha) {
+                System.out.print(coluna+" ");
+            }
+            System.out.println();
+        }
+    }
+}
